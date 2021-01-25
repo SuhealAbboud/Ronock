79ffdf103feee6df513ad7a126edcd65630533d1
Admin <Admin@DESKTOP-MVT5M5Q>
2021-01-26 00:16:24 +0200

    This is coun test

---
+++ Object Repository/Page_Ronock  Landing Page/a_Sign In.rs
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Sign In</name>
+   <tag></tag>
+   <elementGuidId>616dca73-a09c-43a2-bb60-8a4221b2b93f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.d-lg-none.d-inline-block > a.btn.btn--header.btn--signin.mr-3.nav-link</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//a[contains(text(),'Sign In')])[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn--header btn--signin mr-3 nav-link</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/Home/LandingPage</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Sign In</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/header[1]/nav[@class=&quot;navbar navbar-top-default navbar-expand-lg navbar-gradient gradient-bg1 nav-box-round&quot;]/div[@class=&quot;container container--custom&quot;]/div[@class=&quot;d-flex&quot;]/div[@class=&quot;d-lg-none d-inline-block&quot;]/a[@class=&quot;btn btn--header btn--signin mr-3 nav-link&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>(//a[contains(text(),'Sign In')])[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='العربية'])[2]/following::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Get Started'])[1]/following::a[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Get Started'])[2]/preceding::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Why Ronock'])[2]/preceding::a[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[contains(@href, '/Home/LandingPage')])[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/a[2]</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/ronockconcurent/Script1611605959006.groovy
@@ -0,0 +1,61 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.By as By
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.WebElement as WebElement
+import org.openqa.selenium.chrome.ChromeDriver as ChromeDriver
+import java.util.concurrent.TimeUnit as TimeUnit
+
+System.setProperty('webdriver.chrome.driver', 'D:\\2020_sel\\chromedriver.exe')
+
+WebDriver driver = new ChromeDriver()
+
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+
+driver.get('https://partnerwebtest.azurewebsites.net/Ads/CreateEditQuickAd?isFlyerAd=True')
+
+driver.manage().window().maximize()
+
+driver.findElement(By.id('logonIdentifier')).sendKeys('32866f84f9@firemailbox.club')
+
+driver.findElement(By.id('password')).sendKeys('UXYaMHmnWb')
+
+driver.findElement(By.id('next')).click()
+
+//WebUI.navigateToUrl('https://partnerwebtest.azurewebsites.net/Ads/CreateEditQuickAd?isFlyerAd=True\'')
+//driver.get('https://partnerwebtest.azurewebsites.net/Ads/CreateEditQuickAd?isFlyerAd=True')
+
+//driver.navigate().to('https://partnerwebtest.azurewebsites.net/Ads/CreateEditQuickAd?isFlyerAd=True\')
+
+//driver.findElement(By.xpath("//*[text()='Create Quick Flyer']")).click();
+driver.findElement(By.name('AdvertisementTitle')).sendKeys('testflyer')
+
+WebElement a = driver.findElement(By.name('AdvertisementDetails'))
+
+a.sendKeys('this is flyyer desc')
+
+driver.findElement(By.xpath("//div[@class='text']")).click()
+driver.findElement(By.xpath("//div[@class='item']")).click()
+try {
+	Thread.sleep(3000);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+driver.findElement(By.xpath("//button[@id='btnUpSaveGoToStudio']")).click()
+driver.findElement(By.xpath("//button[@class='studio-rightnav__upload']")).click()
+
